f77758a892282e32e6bd6d117bd187d870a585c0
Auss Abbood <a.abbood@live.de>
2024-12-10 14:27:01 +0000

    Remove old files and store 1st experiments with README separately.

---
+++ models/predictions/Llama/own/experiment_1/README.md
@@ -0,0 +1,15 @@
+# Short protocol of 1st experiment
+
+## What did I do?
+In experiment 1, I copied the prompt from the author's Llama2.py script as found in the models/evaluation/LLama folder. Since the few shot and few shot with chain of thought (CoT) experiment were missing in this script, I copied these experiments from the gpt3.5.py code, which is also in the evaluation folder. The few shot experiment included two examples and CoT added reasoning to the answers and added a phrase asking the LLM to reason over its answer.
+
+## Modification of results
+In the few shot settings, llama2 would reiterate the whole conversation with the examples given in the prompt. With a simple regex, I extracted the final answer searching for the last phrase starting with "answer:" and its variations and saved the result in FILENAME_modified.csv.
+
+## Code to produce this result
+A python script is added in this folder to describe the way I did prompting to produce these results.
+
+## Open questions
+- Why is the answer so long for the few shot experiments?
+- Why does performance drop so bad with few shot and more so with CoT?
+- Why are some responses truncated?
---
+++ models/predictions/Llama/own/experiment_1/experiment_1_llama2_eval_own.py
@@ -0,0 +1,223 @@
+import glob
+import json
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+from pathlib import Path
+
+import pandas as pd
+import torch
+from dotenv import load_dotenv
+from tqdm import tqdm
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    LlamaForCausalLM,
+    LlamaPreTrainedModel,
+    LlamaTokenizerFast,
+    PreTrainedTokenizerFast,
+)
+
+load_dotenv()
+CWD = Path().resolve()
+
+
+def prepare_prompt(context: str, question: str, method: str) -> str:
+    match method:
+        case "zero_shot":
+            return f"""
+            Answer the question based on the context provided (take the current year as 2022)
+            generate small answer within 1 to 10 words
+            do not include verb in the answer
+            do not add the phrase Sure! Here's a small answer within 1 to 10 words:
+
+            question : {question}
+            context : ***{context}***
+            answer :"""
+        case "few_shot":
+            return f"""
+            Answer the question based on the context provided (take the current year as 2022)
+            generate only the relevant answer do not include the question in it thus generate small answer within 1 to 10 words.
+            question :
+            How old was Charles Brenton Huggins when he won the Nobel Prize in 1966?
+
+            context : ***category
+            nobel
+
+            Charles Brenton Huggins
+            Born	( | 1901-09-22 | ) | September 22, 1901 | Halifax, Nova Scotia
+            Citizenship	Canadian / American
+            Awards	Nobel Prize for Physiology or Medicine | (1966) | Gairdner Foundation International Award | (1966)
+            ['Charles Brenton Huggins nobel.jpg | Charles Brenton Huggins']***
+            answer : 65 years old
+            ###
+            question : When was the last time that Frederick John Perry won the Davis Cup?
+            context : ***category
+            tennis
+
+            Full name
+            Frederick John Perry
+
+            Died
+            2 February 1995 | (1995-02-02) | (aged 85) | Melbourne, Victoria, Australia
+
+            Team competitions
+            Davis Cup	W | (1933, 1934, 1935, 1936)***
+            answer : 1936
+            ###
+            question : {question}
+            context : ***{context}***
+            answer :"""
+        case "CoT":
+            return f"""
+            Answer the question based on the context provided (take the current year as 2022)
+            generate only the relevant answer do not include the question and unnecessary verbs in it thus generate small answer within 1 to 10 words.
+            Also give the proper reasoning with the answer, first answer then the reason for the generated answer.
+
+            question :
+            How old was Charles Brenton Huggins when he won the Nobel Prize in 1966?
+
+            context : ***category
+            nobel
+
+            Charles Brenton Huggins
+            Born	( | 1901-09-22 | ) | September 22, 1901 | Halifax, Nova Scotia
+            Citizenship	Canadian / American
+            Awards	Nobel Prize for Physiology or Medicine | (1966) | Gairdner Foundation International Award | (1966)
+            ['Charles Brenton Huggins nobel.jpg | Charles Brenton Huggins']***
+            answer and reasoning: 65 years old && Charles was born in 1901 and he won the price in 1966, thus he was 65 years old. (1966 - 1901)
+            ###
+            question : When was the last time that Frederick John Perry won the Davis Cup?
+            context : ***category
+            tennis
+
+            Full name
+            Frederick John Perry
+
+            Died
+            2 February 1995 | (1995-02-02) | (aged 85) | Melbourne, Victoria, Australia
+
+            Team competitions
+            Davis Cup	W | (1933, 1934, 1935, 1936)***
+            answer and reasoning: 1936 && He won the davis cup four times among them 1936 is the lastest year in which he won.
+            ###
+            question : {question}
+            context : ***{context}***
+            answer and reasoning: """
+
+
+def init_model_and_tokenizer() -> tuple[LlamaForCausalLM, LlamaTokenizerFast]:
+    # Reference: https://www.reddit.com/r/huggingface/comments/1bv1kfk/what_is_the_difference_between/
+    model_name = "meta-llama/Llama-2-7b-chat-hf"
+    access_token = os.environ["HF_TOKEN"]
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        device_map="auto",
+        token=access_token,
+        resume_download=None,
+        quantization_config=quantization_config,
+    )  # .to("cuda:1")
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name, use_fast=True, token=access_token, resume_download=None
+    )
+    return model, tokenizer
+
+
+def generate(prompt, model: LlamaPreTrainedModel, tokenizer: PreTrainedTokenizerFast) -> str:
+    # Strip because of: https://huggingface.co/meta-llama/Llama-2-7b-hf
+    message = [
+        {"role": "user", "content": prompt.strip()},
+    ]
+    # Ref: https://huggingface.co/docs/transformers/main/en/chat_templating
+    tokenized_chat = tokenizer.apply_chat_template(message, tokenize=True, return_tensors="pt").to(
+        "cuda:0"
+    )
+    outputs = model.generate(tokenized_chat, max_new_tokens=1000)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True).split("[/INST]")[1]
+
+
+def _load_tables_as_json() -> dict[int, dict]:
+    """Load wiki tables as json into dict with table ID as key and table as value"""
+    id_to_json = {}
+
+    for filename in glob.glob(str(CWD / "../../../data/maindata/tables/json/*")):
+        filename: Path = Path(filename)
+        with open(filename, "r") as f:
+            wiki_table_json = json.load(f)
+            id_of_table = int(filename.stem)
+            id_to_json[id_of_table] = wiki_table_json
+    return id_to_json
+
+
+def _load_qa_dev_set() -> pd.DataFrame:
+    """Load dev set containing questions, answers, and categories that can be linked to tables via their table ID"""
+    return pd.read_csv(CWD / "../../../data/maindata/qapairs/dev-set/dev-set.csv", index_col=0)
+
+
+def _linearize_json_tables(id_to_json: dict[int, dict]) -> dict[int, str]:
+    """Linearizes json Wiki tables to strings
+
+    Note: I checked if my adopted code would produce the same as the original code and it does.
+
+    """
+
+    # Init dict and table IDs
+    tables_as_str = {}
+    table_ids = id_to_json.keys()
+
+    for table_id in tqdm(table_ids, "Linearise tables"):
+        table_as_str = ""
+        table = id_to_json[table_id]
+        for key in table.keys():
+            # JSON Tables are transformed to key + "\n" + value and "\n\n" for new line.
+            if type(table[key]) is type(dict()):
+                # Deals with nested tables.
+                # Several values under same key are added with "\n"
+                # Different keys under key are seperated with "\t"
+                table_as_str = table_as_str + key + "\n"
+                for sub_key in table[key].keys():
+                    if sub_key == key:
+                        table_as_str = table_as_str + str(table[key][sub_key]) + "\n"
+                    else:
+                        table_as_str = table_as_str + sub_key + "\t" + table[key][sub_key] + "\n"
+                table_as_str = table_as_str + "\n"
+            else:
+                table_as_str = table_as_str + key + "\n" + table[key] + "\n\n"
+
+        tables_as_str[table_id] = table_as_str
+
+    return tables_as_str
+
+
+if __name__ == "__main__":
+    id_to_json = _load_tables_as_json()
+    tables_as_str = _linearize_json_tables(id_to_json)
+
+    # Example of nested values
+    id_to_json[998]
+    # Contains nested subkey "Nigerian Civil War": ['Part of Cold War and Decolonisation of Africa', 'Biafra independent state map-en.svg | The de facto independent Republic of Biafra in June 1967']
+    # Value is just appended to the above key without re-mentioning the sub-key
+    tables_as_str[998]
+
+    df_eval = _load_qa_dev_set()
+
+    model, tokenizer = init_model_and_tokenizer()
+    all_output = []
+    for i in tqdm(range(len(df_eval)), "Eval table QA"):
+        table_id = df_eval["table_id"][i]
+        question = df_eval["question"][i]
+
+        context = tables_as_str[table_id]
+        input_text = prepare_prompt(context, question, "CoT")
+        result = generate(input_text, model, tokenizer)
+        all_output.append(result)
+    df_eval["predicted_answer"] = all_output
+    df_eval.to_csv("llama2_own_CoT.csv", index=False, quoting=1)
